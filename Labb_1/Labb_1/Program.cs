443c008cd4a5825338c66c7b37d82c07cc61b39b
FurqanAF <furqan.farooqi@iths.se>
2026-09-15 19:35:32 +0200

    First version labb 1

---
+++ Labb_1/Labb_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+static string myLineread()
+{
+    string inputtedString = Console.ReadLine();
+    return inputtedString;
+       
+        }
+
+Console.WriteLine("Enter a string:");
+string myString = myLineread();
+int strlen = myString.Length;
+
+for (int i = 0; i < strlen; i++) 
+{
+    bool isaNumberfirst = char.IsDigit(myString[i]);
+    
+        if (isaNumberfirst is true) 
+    {  // int matchnumber = myString[i] - '0';
+        char checkNumberfirst = myString[i];
+
+        for (int j = i + 1; j < strlen - 1; j++) 
+        {
+            if ((myString[i] != myString[j] && char.IsDigit(myString[j]) == true))
+                continue;
+            else if ((char.IsDigit(myString[j]) == false))
+            {
+                break;
+            }
+            else if (myString[i] == myString[j]) 
+            {
+                Console.Write(myString[..i]);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.Write(myString[i..(j+1)]);
+
+                Console.ResetColor();
+
+                Console.WriteLine(myString[(j + 1)..]);
+                break;
+            }
+
+        }
+            }
+    }
